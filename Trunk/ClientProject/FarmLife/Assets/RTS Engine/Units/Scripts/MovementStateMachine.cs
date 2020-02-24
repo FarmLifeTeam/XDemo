35b97e03c0deddb1c831135f37303247c01562a3
FarmLife <collapsing2020@outlook.com>
2020-02-24 15:45:26 +0800

    提交工程

---
+++ Trunk/ClientProject/FarmLife/Assets/RTS Engine/Units/Scripts/MovementStateMachine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* One Shot State Machine script created by Oussama Bouanani, SoumiDelRio.
+ * This script is part of the Unity RTS Engine */
+
+namespace RTSEngine
+{
+	public class MovementStateMachine : StateMachineBehaviour {
+
+		//allows the unit to know when it enters and exists the movement state
+		override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+			animator.SetBool ("InMvtState", true);
+        }
+
+		//allows the unit to know when it enters and exists the movement state
+		override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+			animator.SetBool ("InMvtState", false);
+        }
+	}
+}
